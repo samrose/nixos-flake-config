6dd83d70e13b2728863ac1272e0c8658cfbdbc0e
Sam Rose <sam@floxdev.com>
2023-05-07 14:04:09 -0400

    nonec2 nomad

--- flake.nix
+++ flake.nix
@@ -1,5 +1,5 @@
 {
-  description = "NixOS postgrest configuration";
+  description = "NixOS configuration";
 
   inputs.nixpkgs.url = "nixpkgs/nixos-unstable";
 
@@ -8,55 +8,11 @@
     nixpkgs,
   }: let
     system = "x86_64-linux";
-    # System types to support.
-    supportedSystems = ["x86_64-linux" "x86_64-darwin" "aarch64-linux" "aarch64-darwin"];
-
-    # Helper function to generate an attrset '{ x86_64-linux = f "x86_64-linux"; ... }'.
-    forAllSystems = nixpkgs.lib.genAttrs supportedSystems;
-
-    # Nixpkgs instantiated for supported system types.
-    nixpkgsFor = forAllSystems (system:
-      import nixpkgs {
-        inherit system;
-        overlays = [self.overlay];
-      });
+    lib = nixpkgs.lib;
   in {
-    # A Nixpkgs overlay.
-    overlay = final: prev: {
-      postgrest = with final;
-        stdenv.mkDerivation rec {
-          pname = "postgrest";
-          version = "0.0.0";
-          src = fetchurl {
-            url = "https://github.com/PostgREST/postgrest/releases/download/v10.1.0/postgrest-v10.1.0-linux-static-x64.tar.xz";
-            sha256 = "sha256-n8kigbECI0g3fQKinwawfqxjumRy5slndd4Ff+yj91A=";
-          };
-          sourceRoot = ".";
-          #setSourceRoot = "sourceRoot=`pwd`";
-          # Add runtime dependencies to buildInputs.
-          buildInputs = [];
-
-          # Add runtime dependencies required by packages that
-          # depend on this package to propagatedBuildInputs.
-          propagatedBuildInputs = [];
-
-          # Add buildtime dependencies (not required at runtime)
-          # to nativeBuildInputs.
-          nativeBuildInputs = [];
-          installPhase = ''
-            runHook preInstall
-            mkdir -p $out/bin
-            cp -r . $out/bin
-            runHook postInstall
-          '';
-        };
-    };
-    packages = forAllSystems (system: {
-      inherit (nixpkgsFor.${system}) postgrest;
-    });
     nixosConfigurations = {
-      nomad-nginx-server = nixpkgs.lib.nixosSystem {
-        inherit ;
+      nomad-nginx-server = lib.makeOverridable nixpkgs.lib.nixosSystem {
+        inherit system;
         specialArgs = {inherit inputs;};
         modules = [
           ({
@@ -64,14 +20,22 @@
             pkgs,
             ...
           }: {
-            imports = ["${modulesPath}/virtualisation/amazon-image.nix"];
-            ec2.hvm = true;
-            networking.firewall.allowedTCPPorts = [80 443];
+            networking.firewall.allowedTCPPorts = [80 443 8080 4646];
             services.nginx.enable = true;
+            services.nomad.enable = true;
+            services.nomad.settings = {
+              # A minimal config example:
+              server = {
+                enabled = true;
+                bootstrap_expect = 1; # for demo; no fault tolerance
+              };
+              client = {
+                enabled = true;
+              };
+            };
           })
         ];
       };
     };
   };
 }
-
